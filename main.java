3d04b9d1b015e2400c8a504282a87f993e344999
mccamej <mccamej@wwu.edu>
2019-04-24 13:01:37 -0700

    Add new file

---
+++ main.java
@@ -0,0 +1,10 @@
+// Authour: Jonathan McCamey
+// class: CSCI 330
+
+
+public class main{
+  public static void main (String[] args)throws Exception{
+    LexicalAnalyzer lexi = new LexicalAnalyzer();
+    lexi.run(args[0]);
+  }
+}
