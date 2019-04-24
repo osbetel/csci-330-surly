383cd41f260e5846ffb2f3fb21e2f296bff4feb5
mccamej <mccamej@wwu.edu>
2019-04-24 13:09:16 -0700

    Upload New File

---
+++ DeleteParser.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class DeleteParser{
+
+  String input;
+
+  public DeleteParser(String input){
+
+  }
+
+  public String parseRelationName(){
+    
+  }
+}
