6dd5d609a6084b572c477a73d2d0f478180ca3fa
mccamej <mccamej@wwu.edu>
2019-04-24 13:08:57 -0700

    Upload New File

---
+++ DestroyParser.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class DestroyParser{
+
+  String input;
+
+  public DestroyParser(String input){
+
+  }
+
+  public String parseRelationName(){
+    
+  }
+}
