f5c23c09c8f68a10709fd9a7bc4de0b5d21151ae
mccamej <mccamej@wwu.edu>
2019-04-24 13:03:36 -0700

    Upload New File

---
+++ SurlyDatabase.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class SurlyDatabase{
+
+  LinkedList relations;
+
+  public Relation getRelation(String name){
+
+  }
+
+  public void destroyRelation(String name){
+
+  }
+
+  public void createRelation(Relation relation){
+    
+  }
+
+}
