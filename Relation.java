868d2a19c7b50285c74008e8446712e1e9615875
mccamej <mccamej@wwu.edu>
2019-04-24 13:07:33 -0700

    Upload New File

---
+++ Relation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+publib class Relation{
+
+  String name;
+  LinkedList schema;
+  LinkedList tuples;
+
+  public void print(){
+
+  }
+
+  public void insert(Tuple tuple){
+
+  }
+
+  public void delete(){
+
+  }
+}
