116c1a2ded66060e4da623051cefcd0e5ac180a9
Andrew <osbetel@gmail.com>
2019-04-26 15:14:54 -0700

    Filled in Relation class
    Added print() (basic implementation for now, will get more specific during testing), insert(), getName(), and delete().
    
    Note delete() is different from destroy() (which is called in SurlyDatabase). Deletion is to remove all data under the Relation, but still keep the Relation in the database. 
    Destroy is to erase the Relation and its contents from the database entirely.

--- src/Relation.java
+++ src/Relation.java
@@ -1,28 +1,39 @@
 import java.util.*;
-import java.io.*;
 import java.lang.*;
 
 public class Relation{
 
-  String name;
-  LinkedList<Attribute> schema;
-  LinkedList<Tuple> tuples;
+    private String name;
+    private LinkedList<Attribute> schema;
+    private LinkedList<Tuple> tuples;
 
-  public Relation(String name, LinkedList schema, LinkedList tuples){
-    this.name = name;
-    this.schema = schema;
-    this.tuples = tuples;
-  }
+    public Relation(String name, LinkedList schema){
+        this.name = name;
+        this.schema = schema;
+        this.tuples = new LinkedList<>();
+    }
 
-  public void print(){
+    public void print() {
+        System.out.println("RELATION ATTRIBUTE FORMAT: " + schema);
+        System.out.println("ITEMS: " + tuples);
+    }
 
-  }
+    public void insert(Tuple tuple) {
+        //Inserting an item into a relation class. eg: if we have tuple
+        //('Bob', 19254, 'Comp. Sci.') under a relation "table" of format
+        //(student name, ID, major), called "StudentData," then we're
+        //Inserting the tuple into the StudentData table.
+        tuples.add(tuple);
+    }
 
-  public void insert(Tuple tuple){
+    public void delete() {
+        //NOT "destroy," destroy removes relation from database entirely.
+        //Delete simply erases all contents under that relation.
+        schema = new LinkedList<>();
+        tuples = new LinkedList<>();
+    }
 
-  }
-
-  public void delete(){
-
-  }
+    public String getName() {
+        return name;
+    }
 }
