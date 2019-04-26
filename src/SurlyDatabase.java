857bbc43b6b7f72abb3b3ae644df2dbb5a13040a
Andrew <osbetel@gmail.com>
2019-04-26 15:12:26 -0700

    Update to SurlyDatabase
    Since LinkedLists do not have a search function, I implemented a binary search [O(log n) time] to retrieve a desired Relation object by name.
    
    (To clarify, you can use LinkedList.indexOf(Object o), but we do not have a reference to a Relation object except for it's String name, so this is not possible.)
    
    Added createRelation() and destroyRelation().

--- src/SurlyDatabase.java
+++ src/SurlyDatabase.java
@@ -1,26 +1,45 @@
 import java.util.*;
-import java.io.*;
 import java.lang.*;
 
 public class SurlyDatabase{
 
-  LinkedList relations;
+    private LinkedList<Relation> relations;
 
-  public SurlyDatabase(){
-    relations = new LinkedList<Relation>();
-    
-  }
+    public SurlyDatabase(){
+        relations = new LinkedList<Relation>();
+    }
 
-  public Relation getRelation(String name){
+    public Relation getRelation(String name) {
+        return relationBinarySearch(name, 0, relations.size() - 1);
+    }
 
-  }
+    private Relation relationBinarySearch(String name, int start, int end) {
 
-  public void destroyRelation(String name){
+        int mid = (start + end) / 2;
+        if (relations.get(mid).getName().equals(name)) {
+            return relations.get(mid);
+        } else if (start < mid && mid < end) {
+            Relation left = relationBinarySearch(name, start, mid);
+            Relation right = relationBinarySearch(name, mid, end);
+            //todo: need to handle case where Relation doesn't exist
+            // or case where multiple copies of the relation exist with
+            // identical names but different attributes.
+            if (left != null) {
+                return left;
+            } else {
+                return right;
+            }
+        }
+        return null;
+    }
 
-  }
+    public void destroyRelation(String name) {
+        Relation target = getRelation(name);
+        relations.remove(target);
+    }
 
-  public void createRelation(Relation relation){
-
-  }
+    public void createRelation(Relation relation) {
+        relations.add(relation);
+    }
 
 }
