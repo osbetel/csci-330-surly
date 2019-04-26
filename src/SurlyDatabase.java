b4f38ea5db40c456502057d1b02c9da9c344e510
Andrew <osbetel@gmail.com>
2019-04-26 16:11:18 -0700

    Added comments for Tuple, SurlyDatabase

--- src/SurlyDatabase.java
+++ src/SurlyDatabase.java
@@ -1,18 +1,44 @@
 import java.util.*;
 import java.lang.*;
 
+/**
+ * SurlyDatabase is meant to be the top-level object that holds all other objects and data
+ * relating to the database.
+ * LexicalAnalyzer has an instance of SurlyDatabase
+ */
 public class SurlyDatabase{
 
     private LinkedList<Relation> relations;
 
-    public SurlyDatabase(){
-        relations = new LinkedList<Relation>();
+    /**
+     * Constructor.
+     */
+    public SurlyDatabase() {
+        relations = new LinkedList<>();
     }
 
+    /**
+     * Returns a Relation object if it is contained in the SurlyDatabase's LinkedList.
+     * @param name The name of the Relation class you want to find/.
+     * @return Relation obj. Should return null if Relation does not exist. //todo
+     */
     public Relation getRelation(String name) {
         return relationBinarySearch(name, 0, relations.size() - 1);
     }
 
+    /**
+     * Because LinkedLists do not have a search function by object fields. ie: it uses
+     * LinkedList.indexOf(Object o), but if you do not have an instance of that object,
+     * you cannot search for it. Additionally, you can't search by object field.
+     * For example, LinkedList.indexOf(o.getName()) is invalid.
+     *
+     * Since this is a relatively small project, it's easier to just write a custom binary
+     * search than to override the Object.equals() method or write an extended LinkedList class.
+     * @param name The name of the Relation we want
+     * @param start 0
+     * @param end size of the LinkedList - 1
+     * @return Returns the desired Relation object. Save that reference.
+     */
     private Relation relationBinarySearch(String name, int start, int end) {
 
         int mid = (start + end) / 2;
@@ -33,11 +59,20 @@ public class SurlyDatabase{
         return null;
     }
 
+    /**
+     * Executes order 66.
+     * Note, destroying removes all the data under a Relation, and removes it from the SurlyDatabase
+     * @param name Name of the Relation to be destroyed.
+     */
     public void destroyRelation(String name) {
         Relation target = getRelation(name);
         relations.remove(target);
     }
 
+    /**
+     * The first galactic empire.
+     * @param relation The Relation object you want to insert into the SurlyDatabase.
+     */
     public void createRelation(Relation relation) {
         relations.add(relation);
     }
--- src/Tuple.java
+++ src/Tuple.java
@@ -1,21 +1,43 @@
 import java.util.*;
-import java.io.*;
 import java.lang.*;
 
+/**
+ * Tuple class holds a LinkedList of AttributeValues.
+ * So logically, if an Attribute class defines attributes for a relation,
+ * the Tuple holds AttributeValues that give values according to that definition.
+ * ie: if a Relation is [name, id, birthday] then tuple has [bob, 123, 01/01/2019]
+ */
 public class Tuple{
 
     private LinkedList<AttributeValue> values;
 
+    /**
+     * Constructor.
+     * @param values Values should be passed in by the calling function,
+     *               In this case it is the InsertParser
+     */
     public Tuple(LinkedList<AttributeValue> values){
         this.values = values;
     }
 
+    /**
+     * Utilizes a binary search as well. For reasoning see SurlyDatabase
+     * @param attrName Attribute name
+     * @return Returns a String of the attribute's value. NOT the AttributeValue obj
+     */
     public String getValue(String attrName){
         //needs to search the Tuple's List of AttributeValues for
         //A given attribute name, and return that value
         return valueBinarySearch(attrName, 0, values.size() - 1);
     }
 
+    /**
+     * The binary search behind getValue()
+     * @param name desired target attribute name
+     * @param start 0
+     * @param end values.size() - 1
+     * @return Returns the attribute values as a String to getValue()
+     */
     private String valueBinarySearch(String name, int start, int end) {
 
         int mid = (start + end) / 2;
@@ -32,5 +54,4 @@ public class Tuple{
         }
         return null;
     }
-
 }
