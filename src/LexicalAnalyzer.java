a0693758a0d42a04317bd3558705bf7adb17415a
Unknown <osbetel@gmail.com>
2019-04-28 19:36:15 -0700

    Pair-prog. merge of Lexical Analyzer
    Functionality was very similar. We decided to just pick one and decide variable names and structure.

--- src/LexicalAnalyzer.java
+++ src/LexicalAnalyzer.java
@@ -1,87 +1,138 @@
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-public class LexicalAnalyzer{
-  public void run(String fileName) throws Exception{
-    File file = fileCheck(fileName);
-    Scanner scan = new Scanner(file);
-    String passIn="";
-    while(scan.hasNextLine()){
-      String line = scan.nextLine();
-      if(line.contains("#")){
-        passIn="";
-      }
-      else if(line.indexOf(';')!=-1){
-        passIn=passIn+line;
-        PassToParse(passIn);
-        passIn="";
-      }
-      else{
-        passIn=passIn+line;
-      }
-    }
-  }
-    public void PassToParse(String line){
-      int action=0;
-      action=actionCheck(line);
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
 
-      if(action==1){
-        RelationParser parse= new RelationParser(line);
-        System.out.println("Creating "+parse.parseRelationName()+" with "+parse.parseAttributeCount()+" attributes." );
-      }
-      else if(action==2){
-        InsertParser parse= new InsertParser(line);
-        System.out.println("Inserting "+parse.parseInsertCount()+" attributes to "+parse.parseRelationName()+"." );
-      }
-      else if(action==3){
-        PrintParser parse= new PrintParser(line);
-        String[] relations=parse.parseRelationNames();
-        System.out.println("Printing "+relations.length+" relations: ");
-        for(String relation:relations){
-          System.out.print(relation+" ");
+/**
+ * Reads the input file and creates + stores a hashmap to function as the database
+ * RELATION, INSERT, and PRINT commands are handled by their respective parsers
+ */
+public class LexicalAnalyzer {
+
+    // top level hashmap has all the relation names as keys,
+    // ie: COURSE, PREREQ, OFFERING, etc. Using database.get("COURSE")
+    // would return the Relation object for COURSE, which contains all the items that fall
+    // under the COURSE Relation.
+    private SurlyDatabase database;
+
+    /**
+     * Constructor.
+     */
+    public LexicalAnalyzer() {
+        this.database = new SurlyDatabase();
+    }
+
+    public void run(String filename) {
+        //do something with file
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(filename));
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);
+            ex.printStackTrace();
         }
-      }
-      else{
-        System.out.println("-1");
-      }
-    }
 
-    public int actionCheck(String line){
-      String[] arr=line.split(" ");
-      if(arr[0].equals("RELATION")){
-        return 1;
-      }
-      else if(arr[0].equals("INSERT")){
-        return 2;
-      }
-      else if(arr[0].equals("PRINT")){
-        return 3;
-      }
-      else{
-        return 0;
-      }
-    }
+        /**
+         * Some general rules for parsing file input:
+         * 1) if a # symbol comes up then disregard everything after and skip to the next line
+         * 2) There's 3 commands, RELATION, INSERT, and PRINT, and they're handled
+         *    by calling their respective parsers
+         * 3) If the line is fully blank, skip it
+         * 4) When we run into a command keyword, we consume until the first instance of a semi-colon ;
+         */
 
-    public File fileCheck(String fileName){
-      boolean doesntExist= true;
-      Scanner reader= new Scanner(System.in);
-      File tfile= null;
-      while(doesntExist){
-        try{
-          tfile=new File(fileName);
-          if(tfile.exists()){
-            doesntExist=false;
-          }
-          else{
-            throw new Exception("File not found");
-          }
+        sc.useDelimiter(";"); //each sc.next consumes the text file up to the next semi-colon
+        while (sc.hasNextLine()) {
+
+            String nextString = sc.next();
+            String commandToParse = "";
+            Scanner sc2 = new Scanner(nextString);
+            while (sc2.hasNextLine()) {
+                String s = sc2.nextLine();
+                if (!s.startsWith("#")) {
+                    commandToParse += s.trim();
+                }
+            }
+
+            if (commandToParse.startsWith("RELATION")) {
+                //Format is as follows: RELATION relationName (attrName1 <dataType> numOfChars,...);
+                RelationParser rp = new RelationParser(commandToParse);
+                String relationName = rp.parseRelationName();
+                int attrCount = rp.parseAttributeCount();
+                String[] attrFormat = rp.getAttrFormat();
+
+                if (!database.containsRelation(relationName)) {
+                    LinkedList<Attribute> attrList = new LinkedList<>();
+                    for (String s : attrFormat) {
+                        //format of s is "CNUM CHAR 8" and attrFormat is [CNUM CHAR 8, TITLE CHAR 30, CREDITS NUM 4]
+                        String[] arr = s.split(" ");
+                        Attribute attribute = new Attribute(arr[0], arr[1], Integer.parseInt(arr[2]));
+                        attrList.add(attribute);
+                    }
+                    database.createRelation(new Relation(relationName, attrList));
+                }
+//                System.out.println();
+            }
+
+            else if (commandToParse.startsWith("INSERT")) {
+                // INSERT relationName [relation attributes]
+                InsertParser ip = new InsertParser(commandToParse);
+                String relationType = ip.parseRelationName();
+                int itemAttrCount = ip.parseAttributeCount();
+
+                //inserts the Item into database under the relationType object
+                if (database.containsRelation(relationType)) {
+                    Relation rel = database.getRelation(relationType);
+                    if (rel.getAttrCount() == itemAttrCount) {
+                        rel.insert(ip.generateTuple(rel.getSchema()));
+                    } else {
+                        System.out.println("Incorrect num of attributes: \"" + commandToParse + "\"");
+                    }
+                }
+            }
+
+            else if (commandToParse.startsWith("DELETE")) {
+                DeleteParser dp = new DeleteParser(commandToParse);
+                String relName = dp.parseRelationName();
+
+                if (database.containsRelation(relName)) {
+                    Relation rel = database.getRelation(relName);
+                    rel.delete();
+                } else {
+                    System.out.println("Relation \"" + relName + "\" is not in the database.");
+                }
+            }
+
+            else if (commandToParse.startsWith("DESTROY")) {
+                DestroyParser dp = new DestroyParser(commandToParse);
+                String relName = dp.parseRelationName();
+
+                if (database.containsRelation(relName)) {
+                    database.destroyRelation(relName);
+                } else {
+                    System.out.println("Relation \"" + relName + "\" is not in the database.");
+                }
+            }
+
+            else if (commandToParse.startsWith("PRINT")) {
+                // PRINT relationName1, relationName2, ...
+                // Should just print all the values in that relation
+                PrintParser pp = new PrintParser(commandToParse);
+                for (String r : pp.parseRelationNames()) {
+                    if (r.equals("CATALOG")) {
+                        database.printCatalog();
+                    } else {
+                        database.printRelation(r);
+                    }
+                }
+                //For testing
+//                System.out.println("\n\n\n\n\n\n\n\n");
+//                database.getRelation("COURSE").delete();
+//                database.printRelations();
+            }
+            else {  //For random or unrecognized, non-legitimate commands, just skips over them
+                continue;
+            }
         }
-        catch(Exception e){
-          System.out.println(e.getMessage()+". Retype in file name : ");
-          fileName=reader.nextLine();
-        }
-      }
-      return tfile;
     }
-
 }
