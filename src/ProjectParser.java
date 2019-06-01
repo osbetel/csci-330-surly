02e8f4ddbd2870edf9e2da4ef7df9db096eb79d9
mccamej <mccamej@wwu.edu>
2019-06-01 14:35:06 -0700

    Update ProjectParser.java

--- src/ProjectParser.java
+++ src/ProjectParser.java
@@ -1,4 +1,3 @@
-
 /*
  * Created by: Andrew Nguyen
  * Date: 2019-05-24
@@ -6,20 +5,65 @@
  * csci-330-surly
  */
 
+/*
+ *Edited by Jonathan McCamey
+ *Date: 2019-06-01
+ *csci-330-surly
+ *Time 14:34
+ *
+ */
+import java.util.*;
+import java.lang.*;
+
+
+
+
 public class ProjectParser {
 
-    private String input;
-    private String[] relationNames;
+  private String input;
+  private String[] attributeNames;
+  private Relation temp;
 
-    public ProjectParser(String input) {
-        this.input = input;
-    }
+  public ProjectParser(String command, Relation table){
+    input = command;
+    temp = table;
+  }
 
-    public String[] parseRelationNames() {
-        if (relationNames == null) {
-            relationNames = input.substring(input.indexOf(" ") + 1).split(", ");
+  private Relation Project(){
+    ParseAttributeNames();
+    LinkedList<Attribute> AttributeList = temp.getSchema()
+    for(AttributeValue colName: AttributeList ){
+      for(String attributeName: attributeNames){
+        if(name.getName().equals(attributeName)){
+          AttributeList.remove(colName);
+          break;
         }
-        return relationNames;
+      }
     }
+    LinkedList<Tuple> tempTuples = temp.getTuples();
+    for(Tuple tuple: tempTuples){
+      for(AttributeValue value: tuple){
+        for(String attributeName: attributeNames){
+          if(value.getName().equals(attributeName)){
+            tuple.remove(value);
+            break;
+          }
+        }
+      }
+    }
+    return temp;
+
+  }
+
+  private void ParseRelationNames(){
+    if (attributeNames == null) {
+            attributeNames = input.substring(input.indexOf(" ") + 1).split(", ");
+    }
+  }
+
+
+
+
+
 
 }
