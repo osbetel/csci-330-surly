a2eea9bc5aef038377ed2a1f1659f45d2c64b0a0
mccamej <mccamej@wwu.edu>
2019-06-01 14:57:59 -0700

    Update ProjectParser.java

--- src/ProjectParser.java
+++ src/ProjectParser.java
@@ -9,7 +9,7 @@
  *Edited by Jonathan McCamey
  *Date: 2019-06-01
  *csci-330-surly
- *Time 14:53
+ *Time 14:57
  *
  */
 import java.util.*;
@@ -39,10 +39,11 @@ public class ProjectParser {
     }
     LinkedList<Tuple> tempTuples = temp.getTuples();
     for(Tuple tuple: tempTuples){
-      for(AttributeValue value: tuple){
-        if(attributeNames.includes(value.getName)==false){
-            tuple.reomve(value);
-        }
+        LinkedList<attributeValue> tempAttributeval=tuple.getValueList();
+        for(AttributeValue value: tempAttributeval){
+            if(attributeNames.includes(value.getName)==false){
+                tuple.reomve(value);
+            }
         }
       }
     return temp;
